7abcee3b19f78fc94a741efa60170c5aadc7c043
michael-916310 <916310@rambler.ru>
2021-03-15 17:05:40 +0400

    feat: добавил серверный код

--- react-apollo/hackernews-react-apollo/src/index.js
+++ react-apollo/hackernews-react-apollo/src/index.js
@@ -4,10 +4,30 @@ import './styles/index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
+// 1
+import {
+  ApolloProvider,
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache
+} from '@apollo/client';
+
+// 2
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000'
+});
+
+// 3
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+});
+
+// 4
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
+  </ApolloProvider>,
   document.getElementById('root')
 );
 
