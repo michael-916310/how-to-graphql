5d12794fdcc7a5c6bc517f88dfa0f7ec1515db32
michael-916310 <916310@rambler.ru>
2021-03-18 17:38:35 +0400

    feat: добавил подписку

--- react-apollo/hackernews-react-apollo/src/index.js
+++ react-apollo/hackernews-react-apollo/src/index.js
@@ -6,6 +6,10 @@ import { BrowserRouter } from 'react-router-dom';
 import { setContext } from '@apollo/client/link/context';
 import { AUTH_TOKEN } from './constants';
 
+import { split } from '@apollo/client';
+import { WebSocketLink } from '@apollo/client/link/ws';
+import { getMainDefinition } from '@apollo/client/utilities';
+
 import reportWebVitals from './reportWebVitals';
 
 // 1
@@ -16,7 +20,6 @@ import {
   InMemoryCache
 } from '@apollo/client';
 
-// 2
 const httpLink = createHttpLink({
   uri: 'http://localhost:4000'
 });
@@ -31,10 +34,37 @@ const authLink = setContext((_, { headers }) => {
   };
 });
 
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:4000/graphql`,
+  options: {
+    reconnect: true,
+    connectionParams: {
+      authToken: localStorage.getItem(AUTH_TOKEN)
+    }
+  }
+});
+
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query);
+    return (
+      kind === 'OperationDefinition' &&
+      operation === 'subscription'
+    );
+  },
+  wsLink,
+  authLink.concat(httpLink)
+);
+
 // 3
+// const client = new ApolloClient({
+//   //link: httpLink,
+//   link: authLink.concat(httpLink),
+//   cache: new InMemoryCache()
+// });
+
 const client = new ApolloClient({
-  //link: httpLink,
-  link: authLink.concat(httpLink),
+  link,
   cache: new InMemoryCache()
 });
 
